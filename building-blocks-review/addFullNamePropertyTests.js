56b106588d8524af3a5644ec6c38a16a924ce791
Kevin <kgavino@ymail.com>
2017-04-13 14:50:43 -0700

    add addFullNamePropertyTests

---
+++ building-blocks-review/addFullNamePropertyTests.js
@@ -0,0 +1,10 @@
+describe("addFullNameProperty", function() {
+  it("should create a fullName property with the firstName and lastName separated by a space", function() {
+    var tim = {
+      firstName: "Tim",
+      lastName: "Goldfish"
+    };
+    addFullNameProperty(tim);
+    expect(tim.fullName).toBe("Tim Goldfish");
+  });
+});
