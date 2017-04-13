879cc7c5e4d030d95a518d165ddaad7c66bf73e2
David Coleman <colemandavid55@gmail.com>
2017-04-13 17:48:56 -0500

    hopefully last adjustment to odd to even

--- intro-arrays/oddToEvenTests.js
+++ intro-arrays/oddToEvenTests.js
@@ -5,7 +5,7 @@ describe("oddToEven", function() {
   it("each element in array should be divisible by 2", function() {
     expect(oddToEven([1,2,3]).every(function(value){ 
       return value % 2 === 0;
-    }).toBe(true);
+    })).toBe(true);
   });
   it("if orignial array element is odd, should be doubled", function() {
     expect(oddToEven([1,2,3])[0]).toEqual(2);
