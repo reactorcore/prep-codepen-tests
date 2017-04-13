ab06e0bf78b71fad450231fd6097b94bf031e1af
Kevin <kgavino@ymail.com>
2017-04-13 12:57:53 -0700

    add areBothOddTests

---
+++ building-blocks-review/areBothOddTests.js
@@ -0,0 +1,14 @@
+describe("areBothOdd", function() {
+  it("should return a boolean", function() {
+    expect(typeof areBothOdd(40, 3)).toEqual("boolean");
+  });
+  it("should return true if both numbers are odd", function() {
+    expect(areBothOdd(41, 3)).toBe(true);
+  });
+  it("should return false if the first number is even", function() {
+    expect(areBothOdd(4, 3)).toBe(false);
+  });
+  it("should return false if the second number is even", function() {
+    expect(areBothOdd(4, 30)).toBe(false);
+  });
+});
