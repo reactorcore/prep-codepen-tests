b81ab517d360b9eb43224f9e72f6af95100bd15a
David Coleman <colemandavid55@gmail.com>
2017-04-13 18:08:03 -0500

    add cons tests

---
+++ intro-arrays/consTests.js
@@ -0,0 +1,8 @@
+describe("cons", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(cons(0,[1,2,3]))).toEqual(true);
+  });
+  it("should add element to the front of the array", function() {
+    expect(cons("greetings",["hi","hello","goodbye")[0]).toEqual("greetings");
+  });
+});
