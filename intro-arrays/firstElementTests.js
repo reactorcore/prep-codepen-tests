c7c0390b0bec11af836bc827912aae76435c6e4d
David Coleman <colemandavid55@gmail.com>
2017-04-13 17:23:08 -0500

    add first element tests

---
+++ intro-arrays/firstElementTests.js
@@ -0,0 +1,8 @@
+describe("firstElement", function() {
+  it("should return the first element of the array", function() {
+    expect(firstElement([1,2,3])).toEqual(1);
+  });
+  it("should return null if the array is empty", function() {
+    expect(JSON.stringify(firstElementk([]))).toBe(JSON.stringify(null));
+  });
+});
