1bd47222598548ccba309edb11126f14c9ff3a3e
David Coleman <colemandavid55@gmail.com>
2017-04-13 17:37:20 -0500

    add odd to even tests

---
+++ intro-arrays/oddToEvenTests.js
@@ -0,0 +1,13 @@
+describe("oddToEven", function() {
+  it("should return an array", function() {
+    expect(Array.isArray(oddToEven([1,2,3]))).toEqual(true);
+  });
+  it("each element in array should be divisible by 2", function() {
+    expect(oddToEven([1,2,3]).every(function(value){ 
+      return value % 2 === 0;
+    }).toBe(true);
+  });
+  it("if orignial array element is odd, should be doubled", function() {
+    expect(oddToEven([1,2,3])[0]).toEqual(2);
+  });
+});
