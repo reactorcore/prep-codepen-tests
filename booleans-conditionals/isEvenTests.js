7a270b404bd0768edbf89d10c9b43a91657d0ff3
David Coleman <colemandavid55@gmail.com>
2017-04-13 14:03:09 -0500

    add isEven tests

---
+++ booleans-conditionals/isEvenTests.js
@@ -0,0 +1,11 @@
+cribe("isEven", function() {
+  it("should return a boolean", function() {
+    expect(typeof isEven(45)).toEqual("boolean");
+  });
+  it("should return true when input is even", function() {
+    expect(isEven(22)).toBe(true);
+  });
+  it("should return false when input is odd", function() {
+    expect(isEven(77)).toBe(false);
+  });
+});
