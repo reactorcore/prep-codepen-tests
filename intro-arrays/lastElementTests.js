f84e59a9f7418abdd20759805bb5a02819377531
David Coleman <colemandavid55@gmail.com>
2017-04-13 17:28:15 -0500

    add last element tests

---
+++ intro-arrays/lastElementTests.js
@@ -0,0 +1,8 @@
+describe("lastElement", function() {
+  it("should return the first element of the array", function() {
+    expect(lastElement([1,2,3])).toEqual(3);
+  });
+  it("should return null if the array is empty", function() {
+    expect(JSON.stringify(lastELement([]))).toBe(JSON.stringify(null));
+  });
+});
