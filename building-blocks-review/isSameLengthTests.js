cbef82cb21821040feace8e02f08e4d89724066f
Kevin <kgavino@ymail.com>
2017-04-13 12:55:09 -0700

    add isSameLengthTests

---
+++ building-blocks-review/isSameLengthTests.js
@@ -0,0 +1,11 @@
+describe("isSameLength", function() {
+  it("should return a boolean", function() {
+    expect(typeof isSameLength("hi", "there")).toEqual("boolean");
+  });
+  it("should return true if the two words are the same length", function() {
+    expect(isSameLength("yes", "you")).toBe(true);
+  });
+  it("should return false if the two words are not the same length", function() {
+    expect(isSameLength("hi", "there")).toBe(false);
+  });
+});
