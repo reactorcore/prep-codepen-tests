ec195e93e2dc55b98c65ceb37d29681a4b6215e0
David Coleman <colemandavid55@gmail.com>
2017-04-13 16:14:04 -0500

    add repeat string tests

---
+++ while/repeatStringTests.js
@@ -0,0 +1,17 @@
+cribe("repeatString", function() {
+  it("should return a string", function() {
+    expect(typeof repeatString("dog")).toEqual("string");
+  });
+  it("should return an empty string for 0", function() {
+    expect(repeatString("dog", 0)).toBe("");
+  });
+  it("should repeat once for 1", function() {
+    expect(repeatString("dog", 1)).toBe("dog");
+  });
+  it("sould repeat twice for 2", function() {
+    expect(repeatString("dog", 2)).toBe("dogdog");
+  });
+  it("should repeat three times for 3", function() {
+    expect(repeatString("dog", 3)).toBe("dogdogdog");
+  });
+});
