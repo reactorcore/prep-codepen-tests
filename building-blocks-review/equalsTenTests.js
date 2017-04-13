131fd6447e1ec3cfc4293496fc9e8157b687ea49
Kevin <kgavino@ymail.com>
2017-04-13 11:23:38 -0700

    add equalsTenTests

---
+++ building-blocks-review/equalsTenTests.js
@@ -0,0 +1,14 @@
+describe("equalsTen", function() {
+  it("should return a boolean", function() {
+    expect(typeof equalsTen(10)).toEqual("boolean");
+  });
+  it("should return true for the number 10", function() {
+    expect(equalsTen(10)).toBe(true);
+  });
+  it("should return false for a number greater than 10", function() {
+    expect(equalsTen(400)).toBe(false);
+  });
+  it("should return false for the number 10", function() {
+    expect(equalsTen(3)).toBe(false);
+  });
+});
