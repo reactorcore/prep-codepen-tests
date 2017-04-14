ff5c8661fdcd69a861d7359b2514f43a4eda021b
David Coleman <colemandavid55@gmail.com>
2017-04-13 20:31:30 -0500

    add tests to representing books suite

--- data-modeling/representingBooksTests.js
+++ data-modeling/representingBooksTests.js
@@ -1,3 +1,51 @@
+describe('makeBook', function() {
+  var resultObject;
+  var resultKeys;
+
+  beforeEach(function() {
+    resultObject = makeBook('testTitle', 'testAuthor');
+    resultKeys = resultObject ? Object.keys(resultObject) : null;
+  });
+
+  it('accepts at least 2 attributes as arguments', function() {
+    expect(makeBook.length).not.toBeLessThan(2);
+  });
+  it('returns an object', function() {
+    expect(typeof resultObject).toBe('object');
+  });
+  it('returns an object with at least two book attributes', function() {
+    expect(resultKeys.length).not.toBeLessThan(2);
+  });
+  it('includes a title', function() {
+    expect(resultKeys).toContain('title');
+  });
+  it('includes an author', function() {
+    expect(resultKeys).toContain('author');
+  });
+});
+
+describe('displayBook', function() {
+  var testBook = {
+    title: 'testTitle',
+    author: 'testAuthor'
+  };
+
+  var resultString; 
+  beforeEach(function() {
+    resultString = displayBook(testBook);
+  })
+
+  it('accepts a book as an argument', function() {
+    expect(displayBook.length).toBe(1);
+  });
+  it('returns a string', function() {
+    expect(typeof resultString).toBe('string');
+  });
+  it('includes the title in the string', function() {
+    expect(resultString).toContain(testBook.title);
+  });
+});
+
 describe('displayBooks', function() {
   var testBookArray = [
     {
