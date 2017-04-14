190dd5b63e983c8b8e183515a79273ee298363f1
David Coleman <colemandavid55@gmail.com>
2017-04-13 20:12:58 -0500

    add count words tests

---
+++ intro-objects/countWordsTests.js
@@ -0,0 +1,26 @@
+describe('countWords', function() {
+  var resultObject1;
+  var resultObject2;
+  beforeEach(function() {
+    resultObject1 = countWords('The quick brown');
+    resultObject2 = countWords('hello hello world hello world');
+  });
+
+  it('returns an object', function() {
+    expect(typeof resultObject1).toBe('object');
+  });
+  it('counts all of the words', function() {
+    expect(Object.keys(resultObject1).length).toBe(3);
+    expect(resultObject1).toEqual({ The: 1, quick: 1, brown: 1 });
+  });
+  it('handles duplicate words correctly', function() {
+    expect(resultObject2).toEqual({ hello: 3, world: 2 });
+  });
+});
+
+describe('countWordsCaseInsensitive', function() {
+  var resultObject = countWordsCaseInsensitive('hello Hello hElLo HELlo');
+  it('is case insensitive', function() {
+    expect(resultObject).toEqual({ hello: 4 });
+  });
+});
