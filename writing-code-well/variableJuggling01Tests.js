816b5c3a36fb9017ae1b5378cebbef5f60cabb31
David Coleman <colemandavid55@gmail.com>
2017-04-18 12:25:05 -0500

    add variable juggling 01 tests

---
+++ writing-code-well/variableJuggling01Tests.js
@@ -0,0 +1,6 @@
+describe('variable juggling with scalars', function() {
+  it('should evaluate value of x correctly', function() {
+    // Note: intentionally hiding the expected value.
+    expect(answer.finalValueOfX === -7).toBe(true);
+  });
+});
