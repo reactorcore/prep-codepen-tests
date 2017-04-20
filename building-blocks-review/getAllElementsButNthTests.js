44a4de5c9dee50712656c39e504d2a9b4b1b64bf
David Coleman <colemandavid55@gmail.com>
2017-04-20 10:45:41 -0500

    fix typo in get all elements but nth tests

--- building-blocks-review/getAllElementsButNthTests.js
+++ building-blocks-review/getAllElementsButNthTests.js
@@ -1,14 +1,17 @@
-describe("getAllElementsButLast", function() {
+describe("getAllElementsButNth", function() {
   it("should return an array", function() {
-    expect(Array.isArray(getAllElementsButLast([4, 5, 6]))).toBe(true);
+    expect(Array.isArray(getAllElementsButNth([4, 5, 6], 2))).toBe(true);
   });
-  it("should return an array with all the elements of the passed in array, except for the last", function() {
-    expect(getAllElementsButLast([4, 5, 6])).toEqual([4, 5]);
+  it("should return an array with all the elements of the passed in array, except for the nth", function() {
+    expect(getAllElementsButNth([4, 5, 6], 0)).toEqual([5, 6]);
   });
   it("should return an empty array when passed in a single element array", function() {
-    expect(getAllElementsButLast([4])).toEqual([]);
+    expect(getAllElementsButNth([4], 0)).toEqual([]);
+  });
+  it("should return a mirror of the original array when passed an n out of range", function() {
+    expect(getAllElementsButNth([4], 10)).toEqual([4]);
   });
   it("should return an empty array when passed in an empty array", function() {
-    expect(getAllElementsButLast([])).toEqual([]);
+    expect(getAllElementsButNth([])).toEqual([]);
   });
 });
